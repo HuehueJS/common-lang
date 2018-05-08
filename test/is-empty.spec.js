067c2ac5f9f9bb56d3e7ea5d115861b1038571d7
Rafael Lima <lima@localhost.localdomain>
2018-05-07 20:35:49 -0400

    tests

---
+++ test/is-empty.spec.js
@@ -0,0 +1,23 @@
+import { expect } from 'chai';
+import { isEmpty } from '../src/main';
+
+describe("#isEmpty", function() {
+    it('when array is empty return true', function(){
+        expect(isEmpty([])).to.equal(true);
+    })
+    it('when array isn\'t empty return false', function(){
+        expect(isEmpty(['Bruce'])).to.equal(false);
+    })
+    it('when object is empty return true', function(){
+        expect(isEmpty({})).to.equal(true);
+    })
+    it('when array isn\'t empty return false', function(){
+        expect(isEmpty({name:'Bruce Banner'})).to.equal(false);
+    })
+    it('when null return true', function(){
+        expect(isEmpty(null)).to.equal(true);
+    })
+    it('when null return true', function(){
+        expect(isEmpty(undefined)).to.equal(true);
+    })
+})
