d90ae6be056bae70b37868847885d8eb5403a320
Éttore Leandro Tognoli <ettore.leandro.tognoli@gmail.com>
2018-11-16 19:05:13 -0200

    arrow functions at tests

--- test/is-empty.spec.ts
+++ test/is-empty.spec.ts
@@ -1,29 +1,29 @@
 import { expect } from 'chai';
 import { isEmpty } from '../src/index';
 
-describe("#isEmpty", function() {
-    it('when array is empty return true', function(){
+describe("#isEmpty", () => {
+    it('when array is empty return true', () => {
         expect(isEmpty([])).to.equal(true);
     })
-    it('when array isn\'t empty return false', function(){
+    it('when array isn\'t empty return false', () => {
         expect(isEmpty(['Bruce'])).to.equal(false);
     })
-    it('when object is empty return true', function(){
+    it('when object is empty return true', () => {
         expect(isEmpty({})).to.equal(true);
     })
-    it('when array isn\'t empty return false', function(){
-        expect(isEmpty({name:'Bruce Banner'})).to.equal(false);
+    it('when array isn\'t empty return false', () => {
+        expect(isEmpty({ name: 'Bruce Banner' })).to.equal(false);
     })
-    it('when null return true', function(){
+    it('when null return true', () => {
         expect(isEmpty(null)).to.equal(true);
     })
-    it('when null return true', function(){
+    it('when null return true', () => {
         expect(isEmpty(undefined)).to.equal(true);
     })
-    it('when 0 return true', function(){
+    it('when 0 return true', () => {
         expect(isEmpty(0)).to.equal(true);
     })
-    it('when 1 return false', function(){
+    it('when 1 return false', () => {
         expect(isEmpty(1)).to.equal(false);
     })
 })
